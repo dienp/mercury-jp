4a2c5567b0bd56fc7e8510409281b256d52a1afb
Dien Phan <dien.phan@pizzahut.io>
2021-02-11 21:24:02 +0700

    Optimized scripts

--- bin/init-adguard.sh
+++ bin/init-adguard.sh
@@ -2,6 +2,14 @@
 mkdir -p ~/home/adguardhome/work
 mkdir -p ~/home/adguardhome/conf
 
+# Deactivate DNSStubListener and update DNS server address.
+mkdir -p /etc/systemd/resolved.conf.d
+mv ./adguardhome.conf /etc/systemd/resolved.conf.d
+mv /etc/resolv.conf /etc/resolv.conf.backup
+ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+systemctl reload-or-restart systemd-resolved
+
+# Run docker container
 docker run --name adguardhome \
     -v ~/home/adguardhome/work:/opt/adguardhome/work \
     -v ~/home/adguardhome/conf:/opt/adguardhome/conf \
