9e0e8fc5c15afd8bb0d4a2b89b56cd384725830e
Dien Phan <dien.phan@pizzahut.io>
2021-02-11 20:58:17 +0700

    Optimized scripts

---
+++ bin/init-adguard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p ~/home/adguardhome/work
+mkdir -p ~/home/adguardhome/conf
+
+docker run --name adguardhome \
+    -v ~/home/adguardhome/work:/opt/adguardhome/work \
+    -v ~/home/adguardhome/conf:/opt/adguardhome/conf \
+    -p 53:53/tcp -p 53:53/udp \
+    -p 80:80/tcp -p 3000:3000/tcp \
+    -p 67:67/udp -p 68:68/tcp -p 68:68/udp \
+    -p 443:443/tcp \
+    -p 853:853/tcp \
+    -d adguard/adguardhome
