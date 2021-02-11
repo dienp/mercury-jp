5b9d49fd50816628559d5493ac80388d76154643
Dien Phan <dien.phan@pizzahut.io>
2021-02-11 14:02:23 +0700

    Fixed paths

--- bin/init-tun.sh
+++ bin/init-tun.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 var_servername="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+docker_compose_file="../docker/docker-compose.yml";
 echo "IP: ${var_servername}"
 echo "PORT: 1195"
-docker-compose run --rm openvpn-tun ovpn_genconfig -u udp://$var_servername:1195
-docker-compose run --rm openvpn-tun touch /etc/openvpn/vars
-docker-compose run --rm openvpn-tun ovpn_initpki nopass
-docker-compose -f "../docker/docker-compose.yml"  up -d openvpn-tun
+echo "Docker compose file: ${docker_compose_file}"
+docker-compose -f ${docker_compose_file} run --rm openvpn-tun ovpn_genconfig -u udp://$var_servername:1195
+docker-compose -f ${docker_compose_file} run --rm openvpn-tun touch /etc/openvpn/vars
+docker-compose -f ${docker_compose_file} run --rm openvpn-tun ovpn_initpki nopass
+docker-compose -f ${docker_compose_file}  up -d openvpn-tun
