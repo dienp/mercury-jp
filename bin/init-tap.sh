48b74a5c25feffb12def6b7c9332ee4050b7eca3
Dien Phan <ptdien.lxag@gmail.com>
2021-02-20 17:04:25 +0700

    Optimized scripts

--- bin/init-tap.sh
+++ bin/init-tap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 var_servername="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+docker_compose_file="../docker/docker-compose.yml";
+
 echo "IP: ${var_servername}"
 echo "PORT: 1194"
-docker_compose_file="../docker/docker-compose.yml";
 echo "Docker compose file: ${docker_compose_file}"
 docker-compose -f ${docker_compose_file} run --rm openvpn-tap ovpn_genconfig -c -t -u udp://$var_servername:1194
 docker-compose -f ${docker_compose_file} run --rm openvpn-tap touch /etc/openvpn/vars
-docker-compose -f ${docker_compose_file} run --rm openvpn-tap ovpn_initpki nopass
+docker-compose -f ${docker_compose_file} -e cn=$var_servername run --rm openvpn-tap ovpn_initpki nopass
 docker-compose -f ${docker_compose_file} up -d openvpn-tap
 ./add-10-random-tap-users.sh
--- bin/init-tun.sh
+++ bin/init-tun.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 var_servername="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+docker_compose_file="../docker/docker-compose.yml";
+
 echo "IP: ${var_servername}"
 echo "PORT: 1195"
-docker_compose_file="../docker/docker-compose.yml";
+echo "Common name: ${cn}"
 echo "Docker compose file: ${docker_compose_file}"
 docker-compose -f ${docker_compose_file} run --rm openvpn-tun ovpn_genconfig -u udp://$var_servername:1195
 docker-compose -f ${docker_compose_file} run --rm openvpn-tun touch /etc/openvpn/vars
-docker-compose -f ${docker_compose_file} run --rm openvpn-tun ovpn_initpki nopass
+docker-compose -f ${docker_compose_file} -e cn=$var_servername run --rm openvpn-tun ovpn_initpki nopass
 docker-compose -f ${docker_compose_file}  up -d openvpn-tun
 ./add-10-random-tun-users.sh
