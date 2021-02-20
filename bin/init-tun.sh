f82934fa9488e53925a30d63fd1f5a3e3ed57e7a
Dien Phan <ptdien.lxag@gmail.com>
2021-02-20 17:20:17 +0700

    Optimized scripts

--- bin/init-tun.sh
+++ bin/init-tun.sh
@@ -8,6 +8,6 @@ echo "Common name: ${cn}"
 echo "Docker compose file: ${docker_compose_file}"
 docker-compose -f ${docker_compose_file} run --rm openvpn-tun ovpn_genconfig -u udp://$var_servername:1195
 docker-compose -f ${docker_compose_file} run --rm openvpn-tun touch /etc/openvpn/vars
-docker-compose -f ${docker_compose_file} -e cn=$var_servername run --rm openvpn-tun ovpn_initpki nopass
+docker-compose -f ${docker_compose_file} run -e cn=$var_servername --rm openvpn-tun ovpn_initpki nopass
 docker-compose -f ${docker_compose_file}  up -d openvpn-tun
 ./add-10-random-tun-users.sh
