2089420058d3e9065d84946d413424992075eab6
Dien Phan <ptdien.lxag@gmail.com>
2021-02-20 18:30:36 +0700

    Optimized scripts

--- bin/add-10-random-users.sh
+++ bin/add-10-random-users.sh
@@ -5,7 +5,6 @@ vpn_type=$1
 
 for i in {1..10}
 do
-    prefix=$(openssl rand -base64 12);
-    var_clientname="${prefix}.${vpn_type}";
+    var_clientname="${i}.${vpn_type}";
     ./add-user.sh $var_clientname $vpn_type
 done
