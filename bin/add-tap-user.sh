e968536816a985d2bd4ce053021996a970d50700
Dien Phan <ptdien.lxag@gmail.com>
2021-02-20 17:58:51 +0700

    Optimized scripts

--- bin/add-tap-user.sh
+++ bin/add-tap-user.sh
@@ -1,7 +1,8 @@
-
 #!/bin/bash
 docker_compose_file="../docker/docker-compose.yml";
-echo "Docker compose file: ${docker_compose_file}"
 mkdir -p ./tap-users
+
+echo "Docker compose file: ${docker_compose_file}"
+
 docker-compose -f ${docker_compose_file} run --rm openvpn-tap easyrsa build-client-full $1 nopass
 docker-compose -f ${docker-compose_file} run --rm openvpn-tap ovpn_getclient $1 > ./tap-users/$1.ovpn
---
+++ bin/init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+var_servername="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+docker_compose_file="../docker/docker-compose.yml";
+
+echo "IP: ${var_servername}"
+
+# tun or tap
+vpn_type=$1
+
+if [ -z "$vpn_type"]; then
+    vpn_type="tap";
+fi
+
+docker_service_name="openvpn-${vpn_type}";
+
+echo "Run docker service: ${docker_service_name}"
+
+if [[ $vpn_type = "tap" ]]
+then
+    docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -c -t -u udp://$var_servername:1194
+    echo "PORT: 1194"
+else
+    docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -u udp://$var_servername:1195
+    echo "PORT: 1195"
+fi
+
+docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} touch /etc/openvpn/vars
+docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_initpki nopass
+docker-compose -f ${docker_compose_file} up -d ${docker_service_name}
+./add-10-random-${vpn_type}-users.sh
+
