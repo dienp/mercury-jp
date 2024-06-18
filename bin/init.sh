af523fd140b939a44e02e0a6685e3d04206e5115
Phan Thanh Điền <dien.phan@yum.com>
2024-06-18 22:49:25 +0700

    Update init.sh

--- bin/init.sh
+++ bin/init.sh
@@ -18,15 +18,15 @@ echo "Run docker service: ${docker_service_name}"
 
 if [[ $vpn_type = "tap" ]]
 then
-    docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -c -t -u udp://$var_servername:1194
+    docker compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -c -t -u udp://$var_servername:1194
     echo "PORT: 1194"
 else
-    docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -u udp://$var_servername:1195
+    docker compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_genconfig -u udp://$var_servername:1195
     echo "PORT: 1195"
 fi
 
-docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} touch /etc/openvpn/vars
-docker-compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_initpki nopass
-docker-compose -f ${docker_compose_file} up -d ${docker_service_name}
+docker compose -f ${docker_compose_file} run --rm ${docker_service_name} touch /etc/openvpn/vars
+docker compose -f ${docker_compose_file} run --rm ${docker_service_name} ovpn_initpki nopass
+docker compose -f ${docker_compose_file} up -d ${docker_service_name}
 ./add-10-random-users.sh $vpn_type
 
